609538ac9ea6dd8f3886b1436d0b7e457f68fb50
Hubert Zambrzycki <107870354+hZambrzycki@users.noreply.github.com>
2023-06-20 14:06:52 +0200

    Add files via upload

---
+++ Exercise05/SCHEMA_EJERCICIO_4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schema_ejercicio_4;
+
+/**
+ *
+ * @author huber
+ */
+public class SCHEMA_EJERCICIO_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
